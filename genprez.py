742a5c041e34f41f59896481ecb5684a933a95a2
FRIPOUILLEJACK <fripouillejack@gmail.com>
2014-09-16 15:51:00 +0200

    New API for GENPREZ
    Add Myapifilms - (IMDB-TMDB-OMDB-MyAPI)

--- genprez.py
+++ genprez.py
@@ -41,57 +41,70 @@ def main():
     imdb = sys.argv[6]
 
     #___ CONNECT IMDB ___#
-    searchIMDB = "http://deanclatworthy.com/imdb/?id=tt%s" % imdb
+    searchIMDB = "http://Adeanclatworthy.com/imdb/?id=tt%s" % imdb
     try:
         data_IMDB = loads(urlopen(searchIMDB).read())
-    except(HTTPError, ValueError):
+    except(HTTPError, ValueError, URLError):
         data_IMDB = ""
         pass
 
     #___ CONNECT TMDB ___#
-    searchTMDB = "http://api.themoviedb.org/3/movie/tt%s?api_"\
+    searchTMDB = "http://Aapi.themoviedb.org/3/movie/tt%s?api_"\
                  "key=%s&language=fr" % (imdb, tmdb_api_key)
     dataTMDB = urllib2.Request(searchTMDB,
                                headers={"Accept": "application/json"})
     try:
         data_TMDB = loads(urllib2.urlopen(dataTMDB).read())
-    except(HTTPError, ValueError):
+    except(HTTPError, ValueError, URLError):
         data_TMDB = ""
         pass
 
     #___ CONNECT OMDB ___#
-    searchOMDB = "http://www.omdbapi.com/?i=tt%s" % imdb
+    searchOMDB = "http://www.Aomdbapi.com/?i=tt%s" % imdb
     try:
         data_OMDB = loads(urlopen(searchOMDB).read())
-    except(HTTPError, ValueError):
+    except(HTTPError, ValueError, URLError):
         data_OMDB = ""
+        pass
+
+    #___ CONNECT MyAPI ___#
+    searchAPI = "http://www.myapifilms.com/imdb?idIMDB=tt%s&format="\
+                "JSON&aka=0&business=0&seasons=0&seasonYear=0&techn"\
+                "ical=0&lang=en-us&actors=F&biography=0&trailer=1&u"\
+                "niqueName=0&filmography=0&bornDied=0&starSign=0&ac"\
+                "torActress=1&actorTrivia=0&movieTrivia=0" % imdb
+    try:
+        data_API = loads(urlopen(searchAPI).read())
+    except(HTTPError, ValueError, URLError):
+        data_API = ""
         pass
 
     #---> Cover <---#
     cover_TMDB = "poster_path"
     cover_OMDB = "Poster"
+    cover_API = "urlPoster"
+
     if (cover_TMDB in data_TMDB):
-        post = "%s" % data_TMDB['poster_path']
         poster = "[img]https://d3gtl9l2a4fn1j.cloudfront.net/t/p/"\
-                 "original"+post+"[/img]"
+                 "original%s[/img]" % data_TMDB['poster_path']
+    elif (cover_OMDB in data_OMDB):
+        poster = "[img]%s[/img]" % data_OMDB['Poster']
+    elif (cover_API in data_API):
+        poster = "[img]%s[/img]" % data_API['urlPoster']
     else:
-        if (cover_OMDB in data_OMDB):
-            poster = "[img]%s[/img]" % data_OMDB['Poster']
-        else:
-            poster = "[img]N/A[/img]"
+        poster = "[img]N/A[/img]"
 
     #---> Overview <---#
     overview_TMDB = "overview"
     overview_OMDB = "Plot"
-    if (overview_TMDB in data_TMDB):
-        resp_TMDB = "%s" % data_TMDB['overview']
-        if ("None" in resp_TMDB):
-            if (overview_OMDB in data_OMDB):
-                overview = "[i]%s[/i]" % data_OMDB['Plot']
-            else:
-                overview = "[i]N/A[/i]"
-        else:
-            overview = "[i]"+resp_TMDB+"[/i]"
+    overview_API = "plot"
+
+    if (overview_TMDB in data_TMDB and "None" not in data_TMDB['overview']):
+        overview = "[i]%s[/i]" % data_TMDB['overview']
+    elif (overview_OMDB in data_OMDB):
+        overview = "[i]%s[/i]" % data_OMDB['Plot']
+    elif (overview_API in data_API):
+        overview = "[i]%s[/i]" % data_API['plot']
     else:
         overview = "[i]N/A[/i]"
 
@@ -99,123 +112,149 @@ def main():
     title_IMDB = "title"
     title_TMDB = "original_title"
     title_OMDB = "Title"
+    title_API = "title"
+
     if (title_IMDB in data_IMDB):
-        title = "[b][u]Title[/u] : [/b] [i]%s[/i]" % data_IMDB['title']
         dir = "%s" % data_IMDB['title']
+        title = "[b][u]Title[/u] : [/b] [i]%s[/i]" % dir
+    elif (title_TMDB in data_TMDB):
+        dir = "%s" % data_TMDB['original_title']
+        title = "[b][u]Title[/u] : [/b] [i]%s[/i]" % dir
+    elif (title_OMDB in data_OMDB):
+        dir = "%s" % data_OMDB['Title']
+        title = "[b][u]Title[/u] : [/b] [i]%s[/i]" % dir
+    elif (title_API in data_API):
+        dir = "%s" % data_API['title']
+        title = "[b][u]Title[/u] : [/b] [i]%s[/i]" % dir
     else:
-        if (title_TMDB in data_TMDB):
-            title = "[b][u]Title[/u] : [/b] [i]%s"\
-                    "[/i]" % data_TMDB['original_title']
-            dir = "%s" % data_TMDB['original_title']
-        else:
-            if (title_OMDB in data_OMDB):
-                title = "[b][u]Title[/u] : [/b] [i]%s[/i]"\
-                        % data_OMDB['Title']
-                dir = "%s" % data_OMDB['Title']
-            else:
-                title = "[b][u]Title[/u] : [/b] [i]N/A[/i]"
-                dir = "N/A"
+        dir = "N/A"
+        title = "[b][u]Title[/u] : [/b] [i]N/A[/i]"
 
     #---> Year <---#
     year_IMDB = "year"
     year_TMDB = "release_date"
     year_OMDB = "Year"
+    year_API = "year"
+
     if (year_IMDB in data_IMDB):
-        year = "[b][u]Production Year[/u] : [/b] [i]%s[/i]"\
+        year = "[b][u]Production Year[/u] : [/b] [i]%s[/i]" \
                % data_IMDB['year']
+    elif (year_OMDB in data_OMDB):
+        year = "[b][u]Production Year[/u] : [/b] [i]%s[/i]" \
+               % data_OMDB['Year']
+    elif (year_TMDB in data_TMDB):
+        year = "[b][u]Production Year[/u] : [/b] [i]%s[/i]" \
+               % data_TMDB['release_date']
+    elif (year_API in data_API):
+        year = "[b][u]Production Year[/u] : [/b] [i]%s[/i]" \
+               % data_API['year']
     else:
-        if (year_OMDB in data_OMDB):
-            year = "[b][u]Production Year[/u] : [/b] [i]%s"\
-                   "[/i]" % data_OMDB['Year']
-        else:
-            if (year_TMDB in data_TMDB):
-                year = "[b][u]Production Year[/u] : [/b] [i]%s"\
-                       "[/i]" % data_TMDB['release_date']
-            else:
-                year = "[b][u]Production Year[/u] : [/b] [i]N/A[/i]"
+        year = "[b][u]Production Year[/u] : [/b] [i]N/A[/i]"
 
     #---> Nationality <---#
     country_IMDB = "country"
     country_TMDB = "production_countries"
+    country_API = "countries"
+    country_OMDB = "Country"
+
     if (country_IMDB in data_IMDB):
         country = "[b][u]Nationality[/u] : [/b] [i]%s[/i]"\
                   % data_IMDB['country']
+    elif (country_API in data_API):
+        countryList = ""
+        for item in data_API['countries']:
+            countryList += "%s, " % item
+        country = "[b][u]Nationality[/u] : [/b] [i]%s[/i]" % countryList
+    elif (country_TMDB in data_TMDB):
+        countryList = ""
+        for item in data_TMDB['production_countries']:
+            countryList += "%s, " % item['name']
+        country = "[b][u]Nationality[/u] : [/b] [i]%s[/i]" % countryList
+    elif (country_OMDB in data_OMDB):
+        country = "[b][u]Nationality[/u] : [/b] [i]%s[/i]"\
+                  % data_OMDB['Country']
     else:
-        if (country_TMDB in data_TMDB):
-            countryList = ""
-            for country in data_TMDB['production_countries']:
-                countryList += "%s, " % country['name']
-            country = "[b][u]Nationality[/u] : [/b] [i]%s[/i]" % countryList
-        else:
-            country = "[b][u]Nationality[/u] : [/b] [i]N/A[/i]"
+        country = "[b][u]Nationality[/u] : [/b] [i]N/A[/i]"
 
     #---> Genres <---#
     genre_IMDB = "genres"
     genre_TMDB = "genres"
     genre_OMDB = "Genre"
+    genre_API = "genres"
+
     if (genre_IMDB in data_IMDB):
         genres = "[b][u]Genre(s) :[/u][/b] [i]%s[/i]" % data_IMDB['genres']
+    elif (genre_TMDB in data_TMDB):
+        genresList = ""
+        for item in data_TMDB['genres']:
+            genresList += "%s, " % item['name']
+        genres = "[b][u]Genre(s) :[/u][/b] [i]%s[/i]" % genresList
+    elif (genre_OMDB in data_OMDB):
+        genres = "[b][u]Genre(s) :[/u][/b] [i]%s[/i]" % data_OMDB['Genre']
+    elif (genre_API in data_API):
+        genresList = ""
+        for item in data_API['genres']:
+            genresList += "%s, " % item
+        genres = "[b][u]Genre(s) :[/u][/b] [i]%s[/i]" % genresList
     else:
-        if (genre_TMDB in data_TMDB):
-            genresList = ""
-            for genres in data_TMDB['genres']:
-                genresList += "%s, " % genres['name']
-            genres = "[b][u]Genre(s) :[/u][/b] [i]%s[/i]" % genresList
-        else:
-            if (genre_OMDB in data_OMDB):
-                genres = "[b][u]Genre(s) :[/u][/b] [i]%s"\
-                         "[/i]" % data_OMDB['Genre']
-            else:
-                genres = "[b][u]Genre(s) :[/u][/b] [i]N/A[/i]"
+        genres = "[b][u]Genre(s) :[/u][/b] [i]N/A[/i]"
 
     #---> Directors <---#
     director_TMDB = "production_companies"
     director_OMDB = "Director"
+    director_API = "directors"
+
     if (director_OMDB in data_OMDB):
-        directors = "[b][u]Director(s)[/u] : [/b] [i]%s"\
-                    "[/i]" % data_OMDB['Director']
+        directors = "[b][u]Director(s)[/u] : [/b] [i]%s[/i]" \
+                    % data_OMDB['Director']
+    elif (director_API in data_API):
+        directorsList = ""
+        for item in data_API['directors']:
+            directorsList += "%s, " % item['name']
+        directors = "[b][u]Director(s)[/u] : [/b] [i]%s[/i]" % directorsList
+    elif (director_TMDB in data_TMDB):
+        directorsList = ""
+        for item in data_TMDB['production_companies']:
+            directorsList += "%s, " % item['name']
+        directors = "[b][u]Director(s)[/u] : [/b] [i]%s[/i]" % directorsList
     else:
-        if (director_TMDB in data_TMDB):
-            directorsList = ""
-            for production_companies in data_TMDB['production_companies']:
-                directorsList += "%s, " % production_companies['name']
-            directors = "[b][u]Director(s)[/u] : [/b] [i]%s"\
-                        "[/i]" % directorsList
-        else:
-            directors = "[b][u]Director(s)[/u] : [/b] [i]N/A[/i]"
+        directors = "[b][u]Director(s)[/u] : [/b] [i]N/A[/i]"
 
     #---> Runtime <---#
     runtime_IMDB = "runtime"
     runtime_TMDB = "runtime"
     runtime_OMDB = "Runtime"
-    if (runtime_OMDB in data_OMDB):
-        resp_OMDB = "%s" % data_OMDB['Runtime']
-        if ("N/A" != resp_OMDB):
-            runtime = "[b][u]Duration[/u] : [color=#980000][i]%s"\
-                      "[/i][/color][/b]" % data_OMDB['Runtime']
-        else:
-            runtime = "[b][u]Duration[/u] : [color=#980000]"\
-                      "[i]N/A[/i][/color][/b]"
+    runtime_API = "runtime"
+
+    if (runtime_OMDB in data_OMDB and "N/A" != data_OMDB['Runtime']):
+        runtime = "[b][u]Duration[/u] : [color=#980000][i]%s"\
+                  "[/i][/color][/b]" % data_OMDB['Runtime']
+    elif (runtime_API in data_API):
+        runtimeList = ""
+        for item in data_API['runtime']:
+            runtimeList += "%s, " % item
+        runtime = "[b][u]Duration[/u] : [color=#980000]"\
+                  "[i]%s[/i][/color][/b]" % runtimeList
+    elif (runtime_IMDB in data_IMDB):
+        runtime = "[b][u]Duration[/u] : [color=#980000]"\
+                  "[i]%s[/i][/color][/b]" % data_IMDB['runtime']
+    elif (runtime_TMDB in data_TMDB and "0" != data_TMDB['runtime']):
+        runtime = "[b][u]Duration[/u] : [color=#980000]"\
+                  "[i]%s[/i][/color][/b]" % data_TMDB['runtime']
     else:
-        if (runtime_IMDB in data_IMDB):
-            runtime = "[b][u]Duration[/u] : [color=#980000]"\
-                      "[i]%s[/i][/color][/b]" % data_IMDB['runtime']
-        else:
-            if (runtime_TMDB in data_TMDB):
-                rsp_TMDB = "%s" % data_TMDB[runtime]
-                if ("0" != rsp_TMDB):
-                    runtime = "[b][u]Duration[/u] : [color=#980000]"\
-                              "[i]%s[/i][/color][/b]" % data_TMDB['runtime']
-                else:
-                    runtime = "[b][u]Duration[/u] : [color=#980000]"\
-                              "[i]N/A[/i][/color][/b]"
-            else:
-                runtime = "[b][u]Duration[/u] : [color=#980000]"\
-                          "[i]N/A[/i][/color][/b]"
+        runtime = "[b][u]Duration[/u] : [color=#980000]"\
+                  "[i]N/A[/i][/color][/b]"
 
     #---> Actors <---#
     actors_OMDB = "Actors"
-    if (actors_OMDB in data_OMDB):
+    actors_API = "actors"
+
+    if (actors_API in data_API):
+        actorsList = ""
+        for item in data_API['actors']:
+            actorsList += "%s, " % item['actorName']
+        actors = "[spoil=Casting][i]%s[/i][/spoil]" % actorsList
+    elif (actors_OMDB in data_OMDB):
         actors = "[spoil=Casting][i]%s[/i][/spoil]" % data_OMDB['Actors']
     else:
         actors = "[spoil=Casting][i]N/A[/i][/spoil]"
@@ -224,19 +263,22 @@ def main():
     rating_IMDB = "rating"
     rating_TMDB = "vote_average"
     rating_OMDB = "imdbRating"
+    rating_API = "rating"
+
     if (rating_IMDB in data_IMDB):
-        rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s[/i]"\
+        rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s[/i]" \
                  % data_IMDB['rating']
+    elif (rating_OMDB in data_OMDB):
+        rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s[/i]" \
+                 % data_OMDB['imdbRating']
+    elif (rating_TMDB in data_TMDB):
+        rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s[/i]" \
+                 % data_TMDB['vote_average']
+    elif (rating_API in data_API):
+        rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s[/i]" \
+                 % data_API['rating']
     else:
-        if (rating_OMDB in data_OMDB):
-            rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s"\
-                     "[/i]" % data_OMDB['imdbRating']
-        else:
-            if (rating_TMDB in data_TMDB):
-                rating = "[b][u]IMDB Rating[/u] : [/b] [i]%s"\
-                         "[/i]" % data_TMDB['vote_average']
-            else:
-                rating = "[b][u]IMDB Rating[/u] : [/b] [i]N/A[/i]"
+        rating = "[b][u]IMDB Rating[/u] : [/b] [i]N/A[/i]"
 
     #---> DATAS <---#
     imdbLink = "[b][u]IMDB Link[/u] :[/b] [i]"\
@@ -267,7 +309,7 @@ def main():
             % (prezQualite, prezFormat, prezLang, prezSub, prezSize)
 
     if (title_TMDB not in data_TMDB and title_IMDB not in data_IMDB\
-            and title_OMDB not in data_OMDB):
+            and title_OMDB not in data_OMDB and title_API not in data_API):
         print (GREEN+"\n -> "+BLUE+"Genprez ERROR : "+RED+"Movie not found"\
                ", please try again !\n"+END)
     else:
