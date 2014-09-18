8be3611d282408993f18821ac6f19ec9ee36492a
FRIPOUILLEJACK <fripouillejack@gmail.com>
2014-09-18 22:27:30 +0200

    Update LICENSE CeCILL-C
    Open Source Copyright

--- ankoa.py
+++ ankoa.py
@@ -5,43 +5,43 @@
 #     Made with love by grm34 (FRIPOUILLEJACK)     #
 #--------------------------------------------------#
 """
-Copyright PARDO Jérémy (Sept 2014)
-Contact: jerem.pardo@gmail.com
+    Copyright PARDO Jérémy (Sept 2014)
+    Contact: jerem.pardo@gmail.com
 
-This software is a computer program whose purpose is to help command
-line encoders. Intuitive command line interface with many tools:  
+    This software is a computer program whose purpose is to help command
+    line encoders. Intuitive command line interface with many tools:
 
-* FFMPEG easy encoding
-* Thumbnails Generator
-* NFO Generator
-* Genprez Upload
-* Auto make .torrent
+    * FFMPEG easy encoding
+    * Thumbnails Generator
+    * NFO Generator
+    * Genprez Upload
+    * Auto make .torrent
 
-This software is governed by the CeCILL-C license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL-C
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+    This software is governed by the CeCILL-C license under French law and
+    abiding by the rules of distribution of free software.  You can  use,
+    modify and/ or redistribute the software under the terms of the CeCILL-C
+    license as circulated by CEA, CNRS and INRIA at the following URL
+    "http://www.cecill.info".
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
+    As a counterpart to the access to the source code and  rights to copy,
+    modify and redistribute granted by the license, users are provided only
+    with a limited warranty  and the software's author,  the holder of the
+    economic rights,  and the successive licensors  have only  limited
+    liability.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+    In this respect, the user's attention is drawn to the risks associated
+    with loading,  using,  modifying and/or developing or reproducing the
+    software by the user in light of its specific status of free software,
+    that may mean  that it is complicated to manipulate,  and  that  also
+    therefore means  that it is reserved for developers  and  experienced
+    professionals having in-depth computer knowledge. Users are therefore
+    encouraged to load and test the software's suitability as regards their
+    requirements in conditions enabling the security of their systems and/or
+    data to be ensured and,  more generally, to use and operate it in the
+    same conditions as regards security.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL-C license and that you accept its terms.
+    The fact that you are presently reading this means that you have had
+    knowledge of the CeCILL-C license and that you accept its terms.
 """
 
 import os
@@ -1326,7 +1326,7 @@ def main():
 
     if (len(nfoimdb) == 7 and nfoimdb.isdigit()):
         # Search Release Title (IMDB-OMDB-TMDB-MyAPIFilms)
-        
+
         searchIMDB = "http://deanclatworthy.com/imdb/?id=tt"+nfoimdb
         try:
             data1 = loads(urlopen(searchIMDB).read())
