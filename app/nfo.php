866ef8c376759bd9ac880099b2dcf01f72272282
FRIPOUILLEJACK <fripouillejack@gmail.com>
2014-09-21 01:54:26 +0200

    PEP8ify & clean all code !

--- app/nfo.php
+++ app/nfo.php
@@ -16,7 +16,7 @@
 
 // This software is governed by the CeCILL-C license under French law and
 // abiding by the rules of distribution of free software.  You can  use,
-// modify and/ or redistribute the software under the terms of the CeCILL-C
+// modify and/or redistribute the software under the terms of the CeCILL-C
 // license as circulated by CEA, CNRS and INRIA at the following URL
 // "http://www.cecill.info".
 
