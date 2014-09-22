b151b90df71a61e8ed0648b1de2a039f442febf7
FRIPOUILLEJACK <fripouillejack@gmail.com>
2014-09-23 01:25:49 +0200

    New setup method

--- setup.py
+++ setup.py
@@ -55,69 +55,135 @@ from style import (banner, color)
 
 def main():
 
-    # Auto complete
-    def completer(text, state):
-        return (
-            [entry + "/" for entry in os.listdir(
-                os.path.dirname(
-                    readline.get_line_buffer())
-                ) if entry.startswith(text)][state])
+    # HELP
+    usage = "python setup.py [install] | [update]"
+    parser = optparse.OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+    if ((len(args) != 1) and
+            (sys.argv[1] != "install" or sys.argv[1] != "update")):
+        parser.print_help()
+        parser.exit(1)
 
-    readline.parse_and_bind("tab: complete")
-    readline.set_completer(completer)
+    # INSTALL
+    if (sys.argv[1] == "install"):
 
-    # Install form
-    banner()
-    source = raw_input("{0}ENTER SOURCE PATH {1}(ex: /home/user/torrents/)"
-                       "{0} : {2}".format(GREEN, YELLOW, END))
-    result = raw_input("{0}ENTER DESTINATION PATH {1}(ex: /home/user/encod"
-                       "es/){0} : {2}".format(GREEN, YELLOW, END))
-    team = raw_input("{0}ENTER PERSONAL TEAM NAME {1}(ex: KULTURA){0} : {2}"
-                     .format(GREEN, YELLOW, END))
-    readline.parse_and_bind("tab: ")
-    tk = raw_input("{0}ENTER URL TRACKER ANNOUNCE {1}(ex: http://tracker.c"
-                   "om:80/announce){0} : {2}".format(GREEN, YELLOW, END))
-    api = raw_input("{0}ENTER PERSONAL TMDB API KEY {1}(from: https://www."
-                    "themoviedb.org/documentation/api){0} : {2}"
-                    .format(GREEN, YELLOW, END))
+        # Auto complete
+        def completer(text, state):
+            return (
+                [entry + "/" for entry in os.listdir(
+                    os.path.dirname(
+                        readline.get_line_buffer())
+                    ) if entry.startswith(text)][state])
 
-    if (source != "" or result != "" or team != "" or tk != "" or api != ""):
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(completer)
+
+        # Install form
+        banner()
+        source = raw_input("{0}ENTER SOURCE PATH {1}(ex: /home/user/torrent"
+                           "s/){0} : {2}".format(GREEN, YELLOW, END))
+        result = raw_input("{0}ENTER DESTINATION PATH {1}(ex: /home/user/en"
+                           "codes/){0} : {2}".format(GREEN, YELLOW, END))
+        team = raw_input("{0}ENTER PERSONAL TEAM NAME {1}(ex: KULTURA){0} :"
+                         " {2}".format(GREEN, YELLOW, END))
+        readline.parse_and_bind("tab: ")
+        tk = raw_input("{0}ENTER URL TRACKER ANNOUNCE {1}(ex: http://tk.com"
+                       ":80/announce){0} : {2}".format(GREEN, YELLOW, END))
+        api = raw_input("{0}ENTER PERSONAL TMDB API KEY {1}(from: https://w"
+                        "ww.themoviedb.org/documentation/api){0} : {2}"
+                        .format(GREEN, YELLOW, END))
+
+        if (source != "" or result != "" or team != "" or
+                tk != "" or api != ""):
+
+            # AUTHORIZE & CLEAN
+            chmod = ("chmod +x * && cd app/ && chmod +x * && cd ..")
+            rm = ("rm setup.py")
+            os.system(chmod)
+
+            # SAVE personal settings
+            temp = sys.stdout
+            sys.stdout = open('app/save.txt', 'w')
+            print ("{0}|{1}|{2}|{3}|{4}".format(source, result,
+                                                team, tk, api))
+            sys.stdout.close()
+            sys.stdout = temp
+
+            # WRITE personal settings
+            f = file('app/settings.py', 'r')
+            chaine = f.read()
+            f.close()
+            data = chaine.replace("XXX001", source.strip())\
+                         .replace("XXX002", result.strip())\
+                         .replace("XXX003", team.strip().replace(' ', '.'))\
+                         .replace("XXX004", tk.strip())\
+                         .replace("XXX005", api.strip())
+            f = file('app/settings.py', 'w')
+            f.write(data)
+            f.close
+
+            # WRITE nfogen settings
+            ff = file('nfogen.sh', 'r')
+            chaine = ff.read()
+            ff.close()
+            data = chaine.replace("XXX002", result.strip())
+            ff = file('nfogen.sh', 'w')
+            ff.write(data)
+            ff.close
+
+            os.system(rm)
+
+            print ("\n{0} AnkoA {1}-> {2}Installation successful !\n{3}"
+                   .format(BLUE, RED, GREEN, END))
+
+        else:
+            print ("\n{0} AnkoA {1}-> {2}Invalid settings,"
+                   " please try again !\n{3}"
+                   .format(BLUE, GREEN, RED, END))
+
+    # UPDATE
+    if (sys.argv[1] == "update"):
+
         # AUTHORIZE & CLEAN
         chmod = ("chmod +x * && cd app/ && chmod +x * && cd ..")
         rm = ("rm setup.py")
         os.system(chmod)
 
-        # WRITE personal settings
-        f = file('app/settings.py', 'r')
-        chaine = f.read()
-        f.close()
-        data = chaine.replace("XXX001", source.strip())\
-                     .replace("XXX002", result.strip())\
-                     .replace("XXX003", team.strip().replace(' ', '.'))\
-                     .replace("XXX004", tk.strip())\
-                     .replace("XXX005", api.strip())
-        f = file('app/settings.py', 'w')
-        f.write(data)
-        f.close
+        try:
+            # READ personal settings
+            with open('app/save.txt') as save:
+                opts = save.read().split("\n")
 
-        # WRITE nfogen settings
-        ff = file('nfogen.sh', 'r')
-        chaine = ff.read()
-        ff.close()
-        data = chaine.replace("XXX002", result.strip())
-        ff = file('nfogen.sh', 'w')
-        ff.write(data)
-        ff.close
+            # WRITE personal settings
+            f = file('app/settings.py', 'r')
+            chaine = f.read()
+            f.close()
+            data = chaine.replace("XXX001", opts[0].strip())\
+                         .replace("XXX002", opts[1].strip())\
+                         .replace("XXX003", opts[2].strip())\
+                         .replace("XXX004", opts[3].strip())\
+                         .replace("XXX005", opts[4].strip())
+            f = file('app/settings.py', 'w')
+            f.write(data)
+            f.close
 
-        os.system(rm)
+            # WRITE nfogen settings
+            ff = file('nfogen.sh', 'r')
+            chaine = ff.read()
+            ff.close()
+            data = chaine.replace("XXX002", opts[1].strip())
+            ff = file('nfogen.sh', 'w')
+            ff.write(data)
+            ff.close
 
-        print ("\n{0} AnkoA {1}-> {2}Installation successful !\n{3}"
-               .format(BLUE, RED, GREEN, END))
+            os.system(rm)
 
-    else:
-        print ("\n{0} AnkoA {1}-> {2}Invalid settings,"
-               " please try again !\n{3}"
-               .format(BLUE, GREEN, RED, END))
+            print ("\n{0} AnkoA {1}-> {2}Update successful !\n{3}"
+                   .format(BLUE, RED, GREEN, END))
+        except IOError:
+            print ("\n{0} AnkoA {1}-> {2}Update error, fix it"
+                   " with a clean install !\n{3}"
+                   .format(BLUE, GREEN, RED, END))
 
 if (__name__ == "__main__"):
     main()
