1dd42abb10aa78541c007d2c941d83f0735591b6
Jeremy Pardo <grm34@users.noreply.github.com>
2020-06-04 23:15:01 +0200

    Delete pysetup.sh

--- pysetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pacman --noconfirm -Sy git python-pip unzip
-wget https://raw.githubusercontent.com/grm34/AnkoA/master/PyArchboot.zip
-unzip PyArchboot.zip  && rm PyArchboot.zip
-# git clone https://github.com/grm34/PyArchboot.git
-pip install -r ~/PyArchboot/requirements.txt
-echo "alias PyArchboot='cd PyArchboot && python PyArchboot.py'" >> ~/.zshrc
-# shellcheck source=/dev/null
-source ~/.zshrc
