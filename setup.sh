951cd1b292f86d6cb9cf3e5a7f1cdc96f0b8d770
Jeremy Pardo <grm34@users.noreply.github.com>
2020-06-10 19:58:36 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Update pacman cache and install dependencies >>>
+# Update pacman cache and install dependencies
 pacman --noconfirm -Sy git python-pip unzip
 
-# Clone the repository >>>
+# Clone the repository
 wget https://raw.githubusercontent.com/grm34/AnkoA/master/PyArchboot.zip
 unzip PyArchboot.zip  && rm PyArchboot.zip
 # git clone https://github.com/grm34/PyArchboot.git
 
-# Install requirements >>>
+# Install requirements
 pip install -r PyArchboot/requirements.txt
-
-# Move to repository folder
-cd PyArchboot || exit 0
